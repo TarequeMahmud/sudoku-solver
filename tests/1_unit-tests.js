859594843c75d25e3087398b9dbf7ea32b1e1b21
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-18 05:56:26 +0600

    test: add unit test for puzzle string validation logic

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,9 +1,33 @@
-const chai = require('chai');
+const chai = require("chai");
 const assert = chai.assert;
 
-const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+const Solver = require("../controllers/sudoku-solver.js");
+let solver = new Solver();
 
-suite('Unit Tests', () => {
-
+suite("Unit Tests", () => {
+  suite("Unit Test for puzzle string validator logic", () => {
+    test("Logic handles a valid puzzle string of 81 characters", (done) => {
+      const puzzleString1 =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+      assert.isTrue(solver.validate(puzzleString1));
+      done();
+    });
+    test("Logic handles a puzzle string that is not 81 characters in length", (done) => {
+      const puzzleString2 = "14324324";
+      assert.equal(
+        solver.validate(puzzleString2).error,
+        "Expected puzzle to be 81 characters long"
+      );
+      done();
+    });
+    test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", (done) => {
+      const puzzleString2 =
+        "1.5..2.84..63.12.7.#..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+      assert.equal(
+        solver.validate(puzzleString2).error,
+        "Invalid characters in puzzle"
+      );
+      done();
+    });
+  });
 });
