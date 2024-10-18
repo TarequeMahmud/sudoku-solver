ff2a1e1dbccf6a7d2900979fb6c31d7e0c77e8ed
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-18 13:19:57 +0600

    test: add unit test for the logic of checking row placement of value

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -30,4 +30,19 @@ suite("Unit Tests", () => {
       done();
     });
   });
+
+  suite("Unit Test for row placement checking logic", () => {
+    test("Logic handles a valid row placement", (done) => {
+      const puzzleString =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+      assert.equal(solver.checkRowPlacement(puzzleString, 8, 5), true);
+      done();
+    });
+    test("Logic handles an invalid row placement", (done) => {
+      const puzzleString =
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+      assert.equal(solver.checkRowPlacement(puzzleString, 0, 5), false);
+      done();
+    });
+  });
 });
