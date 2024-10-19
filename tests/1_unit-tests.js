47b59bd8293f80d393e83c4ae680280964734eb0
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-19 13:06:39 +0600

    test: add unit test for the logic of checking 9*9-grid-region placement of given value

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -3,12 +3,12 @@ const assert = chai.assert;
 
 const Solver = require("../controllers/sudoku-solver.js");
 let solver = new Solver();
+const puzzleString1 =
+  "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
 
 suite("Unit Tests", () => {
   suite("Unit Test for puzzle string validator logic", () => {
     test("Logic handles a valid puzzle string of 81 characters", (done) => {
-      const puzzleString1 =
-        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
       assert.isTrue(solver.validate(puzzleString1));
       done();
     });
@@ -22,7 +22,8 @@ suite("Unit Tests", () => {
     });
     test("Logic handles a puzzle string with invalid characters (not 1-9 or .)", (done) => {
       const puzzleString2 =
-        "1.5..2.84..63.12.7.#..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
+        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8.-1..16....926914.37.";
+
       assert.equal(
         solver.validate(puzzleString2).error,
         "Invalid characters in puzzle"
@@ -33,30 +34,37 @@ suite("Unit Tests", () => {
 
   suite("Unit Test for row placement checking logic", () => {
     test("Logic handles a valid row placement", (done) => {
-      const puzzleString =
-        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
-      assert.equal(solver.checkRowPlacement(puzzleString, 8, 5), true);
+      assert.equal(solver.checkRowPlacement(puzzleString1, 8, 5), true);
       done();
     });
     test("Logic handles an invalid row placement", (done) => {
-      const puzzleString =
-        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
-      assert.equal(solver.checkRowPlacement(puzzleString, 0, 5), false);
+      assert.equal(solver.checkRowPlacement(puzzleString1, 0, 5), false);
       done();
     });
   });
 
   suite("Unit Test for column placement checking logic", () => {
     test("Logic handles a valid column placement", (done) => {
-      const puzzleString =
-        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
-      assert.equal(solver.checkColPlacement(puzzleString, 7, 1), true);
+      assert.equal(solver.checkColPlacement(puzzleString1, 7, 1), true);
       done();
     });
     test("Logic handles an invalid column placement", (done) => {
-      const puzzleString =
-        "1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.";
-      assert.equal(solver.checkColPlacement(puzzleString, 0, 1), false);
+      assert.equal(solver.checkColPlacement(puzzleString1, 0, 1), false);
+      done();
+    });
+  });
+
+  suite("Unit Test for region placement checking logic", () => {
+    test("Logic handles a valid region (3x3 grid) placement", (done) => {
+      assert.equal(solver.checkRegionPlacement(puzzleString1, 2, 2, "7"), true);
+      done();
+    });
+
+    test("Logic handles a valid region (3x3 grid) placement", (done) => {
+      assert.equal(
+        solver.checkRegionPlacement(puzzleString1, 2, 2, "5"),
+        false
+      );
       done();
     });
   });
