a40233be66d98d1e716489255822ceee475fbe1c
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-22 21:58:00 +0600

    test: add functional tests for POST route for validating the given value against the current state of the board.

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -102,4 +102,187 @@ suite("Functional Tests", () => {
     }
   );
   //suite ends
+
+  suite(
+    "POST /api/check given valid value with valid row and column => responds with its correct placement status",
+    () => {
+      test("Check a puzzle placement with all fields", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle: puzzlesAndSolutions[0][0],
+            coordinate: "A2",
+            value: "3",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.isTrue(res.body.valid, "it should give a valid response");
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with single placement conflict", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle: puzzlesAndSolutions[0][0],
+            coordinate: "A2",
+            value: "9",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.isFalse(res.body.valid, "it should give a invalid response");
+            assert.equal(res.body.conflict.length, 1);
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with multiple placement conflict", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle: puzzlesAndSolutions[0][0],
+            coordinate: "A2",
+            value: "5",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.isFalse(res.body.valid, "it should give a invalid response");
+            assert.isAbove(res.body.conflict.length, 1);
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with all placement conflicts", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle: puzzlesAndSolutions[0][0],
+            coordinate: "A2",
+            value: "2",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.isFalse(res.body.valid, "it should give a invalid response");
+            assert.equal(res.body.conflict.length, 3);
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with missing required fields", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            coordinate: "A2",
+            value: "2",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "Required field(s) missing");
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with invalid characters", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle:
+              "1-5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.",
+            coordinate: "A2",
+            value: "2",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "Invalid characters in puzzle");
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with incorrect length", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle: "14.37.",
+            coordinate: "A2",
+            value: "2",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(
+              res.body.error,
+              "Expected puzzle to be 81 characters long"
+            );
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with invalid placement coordinate", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle: puzzlesAndSolutions[0][0],
+            coordinate: "a2",
+            value: "2",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "Invalid coordinate");
+            done();
+          });
+        //last bracket of the test below
+      });
+      test("Check a puzzle placement with invalid placement value", (done) => {
+        chai
+          .request(server)
+          .keepOpen()
+          .post("/api/check")
+          .send({
+            puzzle: puzzlesAndSolutions[0][0],
+            coordinate: "A2",
+            value: "a",
+          })
+          .end(function (err, res) {
+            assert.equal(res.status, 200);
+
+            assert.isObject(res.body, "response should be an object");
+            assert.equal(res.body.error, "Invalid value");
+            done();
+          });
+        //last bracket of the test below
+      });
+    }
+  );
 });
