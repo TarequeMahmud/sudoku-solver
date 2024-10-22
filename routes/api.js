d60f03dd3b0de7a155db1c5268bc0b437b820052
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2024-10-22 12:01:01 +0600

    feat: Add POST route for solving the puzzle

--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,26 @@
-'use strict';
+"use strict";
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require("../controllers/sudoku-solver.js");
+const rowMap = {
+  A: 0,
+  B: 1,
+  C: 2,
+  D: 3,
+  E: 4,
+  F: 5,
+  G: 6,
+};
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route("/api/check").post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route("/api/solve").post((req, res) => {
+    const puzzleString = req.body.puzzle;
+    const solvePuzzle = solver.solve(puzzleString);
+    if (typeof solvePuzzle === "object" && solvePuzzle.error)
+      return res.json(solvePuzzle);
+    res.json({ solution: solvePuzzle });
+  });
 };
